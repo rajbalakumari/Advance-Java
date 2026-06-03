2f8efff4d4bdd9d2038a55ccc6b9059c76543df9
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 14:54:03 +0530

    Create DBUtil.java

---
+++ JDBC_MinerProject/com.nit.crud/DBUtil.java
@@ -0,0 +1,22 @@
+package com.nit.crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String url="jdbc:mysql://localhost:3306/jdbc_minerproject";
+    private static final String user_name="root";
+    private static final String password="Subham@123";
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException
+    {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	Connection conn=DriverManager.getConnection(url, user_name, password);
+    	return conn;
+    }
+      
+      
+      
+      
+}
