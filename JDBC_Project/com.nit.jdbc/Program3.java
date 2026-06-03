bdf8637ea43ced273e882fe1b822bb1debf76f08
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-06-03 14:20:25 +0530

    Create  Program3.java

---
+++ JDBC_Project/com.nit.jdbc/Program3.java
@@ -0,0 +1,26 @@
+package com.nit.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program3 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//1.load The Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//"jdbc:mysql://localhost:3306/jdbc2_db39","root","root"
+		//2.establish the connection
+        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db_54", "root", "Root");
+        //3.Create Statement
+        Statement s=conn.createStatement();
+        //4.execute the query
+        s.execute("DELETE FROM EMP WHERE ID=105");
+        //5.CLOSE THE CONNECTION
+        conn.close();
+        System.out.println("Row has Deleted Sucessfully.....");
+        
+	}
+
+}
